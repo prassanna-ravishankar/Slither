49c018ea28149c0f7a3b3d9dd34d60068108021c
Prassanna Ganesh <prassi89@gmail.com>
2016-04-15 17:03:48 +0200

    PyBind wrapper test

--- pyWrapper/wrapper.cpp
+++ pyWrapper/wrapper.cpp
@@ -1,261 +1,129 @@
-//
-// Created by prassanna on 4/04/16.
-//
+/*
+    example/example7.cpp -- supporting Pythons' buffer protocol
+    Copyright (c) 2015 Wenzel Jakob <wenzel@inf.ethz.ch>
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
 
-
-#include <boost/python.hpp>
-#include <boost/numpy.hpp>
-#include <stdexcept>
-#include <stdio.h>
-
-#include <string>
+#include <pybind11/pybind11.h>
 #include <iostream>
-#include <fstream>
 
-#include "Platform.h"
+using std::cout;
+using std::endl;
 
-#include "Graphics.h"
-#include "dibCodec.h"
+namespace py = pybind11;
 
-#include "Sherwood.h"
+class Matrix {
+public:
+    Matrix(size_t rows, size_t cols) : m_rows(rows), m_cols(cols) {
+        std::cout << "Value constructor: Creating a " << rows << "x" << cols << " matrix " << std::endl;
+        m_data = new float[rows*cols];
+        memset(m_data, 0, sizeof(float) * rows * cols);
+    }
 
-#include "CumulativeNormalDistribution.h"
+    Matrix(const Matrix &s) : m_rows(s.m_rows), m_cols(s.m_cols) {
+        std::cout << "Copy constructor: Creating a " << m_rows << "x" << m_cols << " matrix " << std::endl;
+        m_data = new float[m_rows * m_cols];
+        memcpy(m_data, s.m_data, sizeof(float) * m_rows * m_cols);
+    }
 
-#include "DataPointCollection.h"
+    Matrix(Matrix &&s) : m_rows(s.m_rows), m_cols(s.m_cols), m_data(s.m_data) {
+        std::cout << "Move constructor: Creating a " << m_rows << "x" << m_cols << " matrix " << std::endl;
+        s.m_rows = 0;
+        s.m_cols = 0;
+        s.m_data = nullptr;
+    }
 
-#include "Classification.h"
-#include "DensityEstimation.h"
-#include "SemiSupervisedClassification.h"
-#include "Regression.h"
-#include <boost/program_options.hpp>
+    ~Matrix() {
+        std::cout << "Freeing a " << m_rows << "x" << m_cols << " matrix " << std::endl;
+        delete[] m_data;
+    }
 
-#include "NumPyArrayData.h"
+    Matrix &operator=(const Matrix &s) {
+        std::cout << "Assignment operator : Creating a " << s.m_rows << "x" << s.m_cols << " matrix " << std::endl;
+        delete[] m_data;
+        m_rows = s.m_rows;
+        m_cols = s.m_cols;
+        m_data = new float[m_rows * m_cols];
+        memcpy(m_data, s.m_data, sizeof(float) * m_rows * m_cols);
+        return *this;
+    }
 
-namespace bp = boost::python;
-namespace np = boost::numpy;
-using namespace MicrosoftResearch::Cambridge::Sherwood;
-
-#define ASSERT_THROW(a,msg) if (!(a)) throw std::runtime_error(msg);
-
-
-//Defaults
-std::auto_ptr<DataPointCollection> test_train_data;
-std::auto_ptr<Forest<LinearFeatureResponseSVM, HistogramAggregator> > forest;
-TrainingParameters trainingParameters;
-LinearFeatureSVMFactory featureFactory;
-
-np::ndarray divideByTwo(const np::ndarray& arr)
-{
-    ASSERT_THROW( (arr.get_nd() == 2), "Expected two-dimensional array");
-    ASSERT_THROW( (arr.get_dtype() == np::dtype::get_builtin<double>()), "Expected array of type double (np.float64)");
-
-    np::ndarray result = np::zeros(bp::make_tuple(arr.shape(0),arr.shape(1)), np::dtype::get_builtin<double>());
-
-    NumPyArrayData<double> arr_data(arr);
-    NumPyArrayData<double> result_data(result);
-
-    for (int i=0; i<arr.shape(0); i++) {
-        for (int j=0; j<arr.shape(1); j++) {
-            result_data(i,j) = arr_data(i,j) / 2.0;
+    Matrix &operator=(Matrix &&s) {
+        std::cout << "Move assignment operator : Creating a " << s.m_rows << "x" << s.m_cols << " matrix " << std::endl;
+        if (&s != this) {
+            delete[] m_data;
+            m_rows = s.m_rows; m_cols = s.m_cols; m_data = s.m_data;
+            s.m_rows = 0; s.m_cols = 0; s.m_data = nullptr;
         }
+        return *this;
     }
 
-    return result;
-}
-
-bool loadData(const np::ndarray& arr, const np::ndarray& lbls)
-{
-    ASSERT_THROW( (arr.get_nd() == 2), "Expected two-dimensional Data array");
-    ASSERT_THROW( (arr.get_dtype() == np::dtype::get_builtin<double>()), "Expected array of type double (np.float64)");
-    ASSERT_THROW( (lbls.get_dtype() == np::dtype::get_builtin<double>()), "Expected array of type double (np.float64)");
-    ASSERT_THROW( (lbls.get_nd() == 1), "Expected one-dimensional Label array");
-
-
-    NumPyArrayData<double> arr_data(arr);
-    NumPyArrayData<double> lbl_data(lbls);
-    test_train_data = std::auto_ptr<DataPointCollection>(new DataPointCollection());
-    test_train_data->reserve(arr.shape(0), arr.shape(1));
-
-    int count = 0;
-    for(int i = 0;i<arr.shape(0);i++)
-    {
-        for(int j=0;j<arr.shape(1);j++)
-            test_train_data->putValue( (float)arr_data(i,j),
-                                       (int)lbl_data(i),
-                                       i, j);
-            //test_train_data->data_.push_back ((float)arr_data(i,j));
-
-        count++;
-
+    float operator()(size_t i, size_t j) const {
+        return m_data[i*m_cols + j];
     }
 
-
-
-
-    return (count == test_train_data->Count());
-}
-
-
-bool setDefaultParams()
-{
-    //Defaults
-    trainingParameters.MaxDecisionLevels = 10;
-    trainingParameters.NumberOfCandidateFeatures = 10;
-    trainingParameters.NumberOfCandidateThresholdsPerFeature = 10;
-    trainingParameters.NumberOfTrees = 10;
-    trainingParameters.Verbose = true;
-    trainingParameters.svm_c = 0.5;
-    trainingParameters.igType =  ig_shannon;
-    trainingParameters.featureMask = FeatureMaskType::hypercolumn;
-
-}
-
-bool setMaxDecisionLevels(int n)
-{
-    trainingParameters.MaxDecisionLevels = n;
-    return true;
-}
-
-bool setNumberOfCandidateFeatures(int n)
-{
-    trainingParameters.NumberOfCandidateFeatures = n;
-    return true;
-}
-
-bool setNumberOfThresholds(int n)
-{
-    trainingParameters.NumberOfCandidateThresholdsPerFeature = n;
-    return true;
-}
-
-bool setTrees(int n)
-{
-    trainingParameters.NumberOfTrees = n;
-    return true;
-}
-
-bool setQuiet(bool choice)
-{
-    trainingParameters.Verbose = !choice;
-    return true;
-}
-
-bool setSVM_C(float c)
-{
-    trainingParameters.svm_c = c;
-    return true;
-}
-
-bool onlyTrain()
-{
-
-
-    forest = ClassificationDemo<LinearFeatureResponseSVM>::Train(*test_train_data,
-                                                                 &featureFactory,
-                                                                 trainingParameters);
-
-    return true;
-}
-
-
-bool saveModel(std::string filename)
-{
-    forest->Serialize(filename);
-
-    return true;
-}
-
-bool loadModel(std::string filename)
-{
-    forest = Forest<LinearFeatureResponseSVM, HistogramAggregator>::Deserialize(filename);
-
-    return true;
-}
-
-
-
-//For 2 class problems
-np::ndarray onlyTest()
-{
-    int nr_classes = 2;
-    std::vector<HistogramAggregator> distbns;
-    ClassificationDemo<LinearFeatureResponseSVM>::Test(*forest.get(),
-                                                       *test_train_data.get(),
-                                                       distbns);
-
-
-    np::ndarray result = np::zeros(bp::make_tuple(distbns.size(),nr_classes), np::dtype::get_builtin<double>());
-
-
-    NumPyArrayData<double> result_data(result);
-    for (int i=0; i<result.shape(0); i++)
-    {
-            //float sum = distbns[i].bins_[0]+distbns[i].bins_[1];
-            result_data(i,0) = distbns[i].GetProbability(0);
-            result_data(i,1) = distbns[i].GetProbability(1);
-
+    float &operator()(size_t i, size_t j) {
+        return m_data[i*m_cols + j];
     }
 
-    return result;
+    float *data() { return m_data; }
 
+    size_t rows() const { return m_rows; }
+    size_t cols() const { return m_cols; }
+private:
+    size_t m_rows;
+    size_t m_cols;
+    float *m_data;
+};
 
+void init_ex7(py::module &m) {
+    py::class_<Matrix> mtx(m, "Matrix");
+
+    mtx.def(py::init<size_t, size_t>())
+                    /// Construct from a buffer
+            .def("__init__", [](Matrix &v, py::buffer b) {
+                py::buffer_info info = b.request();
+                if (info.format != py::format_descriptor<float>::value() || info.ndim != 2)
+                    throw std::runtime_error("Incompatible buffer format!");
+                new (&v) Matrix(info.shape[0], info.shape[1]);
+                memcpy(v.data(), info.ptr, sizeof(float) * v.rows() * v.cols());
+            })
+
+            .def("rows", &Matrix::rows)
+            .def("cols", &Matrix::cols)
+
+                    /// Bare bones interface
+            .def("__getitem__", [](const Matrix &m, std::pair<size_t, size_t> i) {
+                if (i.first >= m.rows() || i.second >= m.cols())
+                    throw py::index_error();
+                return m(i.first, i.second);
+            })
+            .def("__setitem__", [](Matrix &m, std::pair<size_t, size_t> i, float v) {
+                if (i.first >= m.rows() || i.second >= m.cols())
+                    throw py::index_error();
+                m(i.first, i.second) = v;
+            })
+                    /// Provide buffer access
+            .def_buffer([](Matrix &m) -> py::buffer_info {
+                return py::buffer_info(
+                        m.data(),                              /* Pointer to buffer */
+                        sizeof(float),                         /* Size of one scalar */
+                        py::format_descriptor<float>::value(), /* Python struct-style format descriptor */
+                        2,                                     /* Number of dimensions */
+                        { m.rows(), m.cols() },                /* Buffer dimensions */
+                        { sizeof(float) * m.rows(),            /* Strides (in bytes) for each index */
+                          sizeof(float) }
+                );
+            });
 }
 
 
+PYBIND11_PLUGIN(rfsvm) {
+    py::module m("rfsvm", "pybind rfsvm plugin");
+
+    init_ex7(m);
 
 
-bp::tuple createGridArray(int rows, int cols)
-{
-    np::ndarray xgrid = np::zeros(bp::make_tuple(rows, cols), np::dtype::get_builtin<int>());
-    np::ndarray ygrid = np::zeros(bp::make_tuple(rows, cols), np::dtype::get_builtin<int>());
-
-    NumPyArrayData<int> xgrid_data(xgrid);
-    NumPyArrayData<int> ygrid_data(ygrid);
-
-    for (int i=0; i<rows; i++) {
-        for (int j=0; j<cols; j++) {
-            xgrid_data(i,j) = i;
-            ygrid_data(i,j) = j;
-        }
-    }
-
-    return bp::make_tuple(xgrid,ygrid);
-}
-
-BOOST_PYTHON_MODULE(Rfsvm)
-{
-    np::initialize();
-
-    bp::def("loadData", loadData, bp::args("Features", "Labels"));
-    bp::def("onlyTrain", onlyTrain);
-    bp::def("setDefaultParams", setDefaultParams);
-    bp::def("onlyTest", onlyTest);
-    bp::def("saveModel", saveModel);
-    bp::def("loadModel", loadModel);
-    bp::def("setMaxDescionLevels", setMaxDecisionLevels);
-    bp::def("setNumberOfCandidateFeatures",setNumberOfCandidateFeatures);
-    bp::def("setNumberOfThresholds",setNumberOfThresholds);
-    bp::def("setTrees",setTrees);
-    bp::def("setQuiet",setQuiet);
-    bp::def("setSVM_C",setSVM_C);
-
-}
-
-
-int main(int argc, char **argv)
-{
-    // This line makes our module available to the embedded Python intepreter.
-# if PY_VERSION_HEX >= 0x03000000
-    PyImport_AppendInittab("Rfsvm", &PyInit_example);
-# else
-    PyImport_AppendInittab("Rfsvm", &initexample);
-# endif
-    // Initialize the Python runtime.
-    Py_Initialize();
-
-    PyRun_SimpleString(
-            "import Rfsvm\n"
-                    "import numpy\n"
-                    );
-    Py_Finalize();
+    return m.ptr();
 }
